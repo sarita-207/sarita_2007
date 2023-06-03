fe6e06f2b3aa17331e0b974745d53b7ddce59e7f
sarita-207 <ingridcanas2007@gmail.com>
2023-06-03 15:05:05 -0500

    agregue el boton has olvidado la contraseña

--- script.js
+++ script.js
@@ -1,32 +1,60 @@
-// JSON de usuarios registrados (solo como ejemplo)
+// Clase Usuario
+class UsuarioFacebook {           
+  constructor(usuario,contraseña) { //<TAREA - agregar con una coma contraseña> (usuario, contraseña)  
+    this.usuario = usuario; //ejemplo de que a la clase UsuarioFacebook se le agrega un atributo usuario
+    this.contraseña = contraseña
+  }
+}
+
+
+
+
+//lista de usuarios
 var users = [
-    { username: "usuario1", password: "contraseña1" },
-    { username: "usuario2", password: "contraseña2" },
-    { username: "usuario3", password: "contraseña3" }
-  ];
-  
-  // Función para validar las credenciales
-  function validarCredenciales() {
-    var username = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-  
-    // Buscar el usuario en el JSON
-    var usuarioValido = users.find(function(user) {
-      return user.username === username && user.password === password;
-    });
-  
-    if (usuarioValido) {
-      // El usuario ha iniciado sesión correctamente
-      alert("¡Inicio de sesión exitoso!");
-    } else {
-      // Las credenciales son inválidas
-      alert("Nombre de usuario o contraseña incorrectos.");
+  new UsuarioFacebook("usuario1", "contraseña1"),
+  new UsuarioFacebook("usuario2", "contraseña2"),
+  new UsuarioFacebook("juan", "123") //TAREA - Agregar Nuevo usuarios con usuario: juan y contraseña: 123
+]; 
+
+
+
+
+// Función para saber si el usuario existe
+function validarCredenciales() {
+  var username = document.getElementById("email").value; //obtener el valor del input con id="email"
+  //TAREA - Agregar el valor del input con id="contraseña" a una variable llamada contraseña
+  var contraseña = document.getElementById("contraseña").value;
+
+
+
+
+  //una variable con valor false
+  var usuarioValido = false;
+
+  // Buscar el usuario en el JSON logica que veremos 
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].usuario == username  && users[i].contraseña === contraseña) {
+      usuarioValido = true;
     }
   }
+
+  return usuarioValido;
+}
+
+
+// Función para mostrar alerta
+function alertaCredeciales(event) {
+  event.preventDefault(); //evitar que se recargue la página
   
-  // Asignar la función de validación al formulario
-  document.getElementById("login-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evitar envío del formulario
-    validarCredenciales();
-  });
-  
+  if (validarCredenciales() == false) { //si la funcion validarCredenciales es true
+    alert("Credenciales incorrectas!");
+  }
+  if (validarCredenciales() == true) { 
+    alert("Credenciales correctas!");
+  }
+  // TAREA - agregar if en caso de true
+}
+
+
+
+document.getElementById("login-button").addEventListener("click", alertaCredeciales); //agregar el evento click al boton con id="login-button"
